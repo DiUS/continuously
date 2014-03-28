cf5f8f06771ea833061f38aaaf65ac1ad106258b
Elliott Murray <emurray@dius.com.au>
2014-03-28 12:13:11 +1100

    rails app

---
+++ continuously/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Continuously::Application.config.secret_key_base = 'c0bb93eaaaf3cda665d3ad86e50e6b96952df527a3b315509b06191e15556f5b918afb7885b701dee06e4139fd35e166dc98f9528c29fc80407cf94b4d2c22e7'
---
+++ continuously/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Continuously::Application.config.session_store :cookie_store, key: '_continuously_session'
