191e9310ed102c195cebbefa8556f2430b3d6387
Lincoln Fitzsimons <lincolnf@dius.com.au>
2014-03-28 16:10:22 +1100

    docker_peer can now build and run docker

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'mixlib-shellout'
-
+gem 'docker-api', :require => 'docker'
 gem 'docker_peer', path: './continuously/lib/docker_peer'
--- continuously/lib/docker_peer/lib/docker_peer.rb
+++ continuously/lib/docker_peer/lib/docker_peer.rb
@@ -5,12 +5,19 @@ class DockerPeer
   def run(path) 
     runner_conf = YAML.load_file("#{path}/test.yml")
     puts runner_conf
-#    runner_conf['system'].each do |container| 
-#      build_path = container['build']
-#      image = Docker::Image.build_from_dir(build_path)
-#      Docker::Container.create('Image' => image) 
-#      
-#    end
+    containers = []
+    runner_conf['system'].each do |container_conf| 
+      build_path = container_conf['build']
+      puts build_path
+      puts Docker.inspect
+      image = Docker::Image.build_from_dir(build_path)
+      containers <<  Docker::Container.create('Image' => image.id, 'Cmd' => [container_conf['cmd']], 'Volumes' => container_conf['volumes'])#, 'VolumesFrom' => container_conf['volumes-from']) 
+      containers.last.start
+      puts containers.last.json
+    end
+    containers.each do |container|
+      container.wait
+    end
   end
 end
 
