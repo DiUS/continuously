382a7a0566efa806b62153d302b798dbbbaaad27
Warner Godfrey <w@godfrey.io>
2014-03-28 14:46:40 +1100

    using container volume that Kevin has set-up

--- continuously/app/controllers/git_controller.rb
+++ continuously/app/controllers/git_controller.rb
@@ -8,11 +8,11 @@ class GitController < ActionController::Base
     name = payload["repository"]["name"]
     url = payload["repository"]["url"]
     sha = payload["head_commit"]["id"]
-    home_dir = "/var/lib/continuously"
-    path = "#{home_dir}/#{name}"
+    home_dir = "/var/containers"
+    repo_dir = "#{home_dir}/#{name}"
     # Clone and/or pull
-    if File.directory?(path)
-      repo = Git.open(path)
+    if File.directory?(repo_dir)
+      repo = Git.open(repo_dir)
       repo.pull # We should pull to the version that was pushed, here
     else
       repo = Git.clone(url, name, :path => home_dir) # version?
