4bc91d69e4a49660d20b921b7ecab38128442633
Warner Godfrey <w@godfrey.io>
2014-03-28 14:43:50 +1100

    git webhook endpoint now clones or pulls the repo

--- continuously/app/controllers/git_controller.rb
+++ continuously/app/controllers/git_controller.rb
@@ -1,7 +1,22 @@
+require 'git'
+require 'docker'
+
 class GitController < ActionController::Base
+  respond_to :json
   def create
-    push = JSON.parse(params[:payload])
-    puts "I got some JSON: #{push.inspect}"
+    payload = JSON.parse(params[:payload])
+    name = payload["repository"]["name"]
+    url = payload["repository"]["url"]
+    sha = payload["head_commit"]["id"]
+    home_dir = "/var/lib/continuously"
+    path = "#{home_dir}/#{name}"
+    # Clone and/or pull
+    if File.directory?(path)
+      repo = Git.open(path)
+      repo.pull # We should pull to the version that was pushed, here
+    else
+      repo = Git.clone(url, name, :path => home_dir) # version?
+    end
     render json: {}
   end
 end
