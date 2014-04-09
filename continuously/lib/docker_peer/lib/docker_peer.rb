20344befc52300fbde68cbeba508fd84c50bd1b9
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-04-10 06:54:10 +1000

    threading to build all at once, and better output of what's going on

--- continuously/lib/docker_peer/lib/docker_peer.rb
+++ continuously/lib/docker_peer/lib/docker_peer.rb
@@ -4,20 +4,27 @@ require 'docker'
 class DockerPeer
   def run(path) 
     runner_conf = YAML.load_file("#{path}/test.yml")
-    puts runner_conf
-    containers = []
-    runner_conf['system'].each do |container_conf| 
-      build_path = container_conf['build']
-      puts build_path
-      puts Docker.inspect
-      image = Docker::Image.build_from_dir(build_path)
-      containers <<  Docker::Container.create('Image' => image.id, 'Cmd' => [container_conf['cmd']], 'Volumes' => container_conf['volumes'])#, 'VolumesFrom' => container_conf['volumes-from']) 
-      containers.last.start
-      puts containers.last.json
+    threads = []
+    trap 'SIGTERM' do
+      puts "Killing #{threads.size} threads"
+      threads.each { |thr| thr.terminate }
     end
-    containers.each do |container|
-      container.wait
+    runner_conf.fetch('system', []).each do |container_conf| 
+      threads << Thread.new do
+        build_path = container_conf['build']
+        puts "Building #{build_path}"
+        image = Docker::Image.build_from_dir(build_path)
+        puts "Running #{image}"
+        container = Docker::Container.create('Image' => image.id,
+          'Cmd' => [container_conf['cmd']],
+          'Volumes' => container_conf['volumes'],
+          'VolumesFrom' => container_conf['volumes-from']) 
+        container.tap(&:start).attach { |stream, chunk| puts "#{stream}: #{chunk}" }
+        container.wait
+        Thread.exit
+      end
     end
+    threads.each { |thr| thr.join }
   end
 end
 
