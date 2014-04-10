cf2f0bbe40977bd97eae194ea2f9326458b5b0ec
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-04-10 14:22:22 +0000

    making the build complete successfully

--- continuously/lib/docker_peer/lib/docker_peer.rb
+++ continuously/lib/docker_peer/lib/docker_peer.rb
@@ -1,30 +1,67 @@
 require 'yaml'
 require 'docker'
+require 'json'
 
 class DockerPeer
-  def run(path) 
+  def initialize
+    @images = {}
+    @system_containers = []
+    @test_containers = []
+  end
+
+  def run(path)
     runner_conf = YAML.load_file("#{path}/test.yml")
-    threads = []
-    trap 'SIGTERM' do
-      puts "Killing #{threads.size} threads"
-      threads.each { |thr| thr.terminate }
+    system_confs = runner_conf.fetch('system', [])
+    test_confs = runner_conf.fetch('test', [])
+    all_confs = system_confs + test_confs
+    # Not threaded because it changes directories
+    all_confs.each { |conf| @images[conf['build']] = build_image(conf['build']) }
+    thread_containers(system_confs, @system_containers)
+    thread_containers(test_confs, @test_containers)
+    # Wait for tests to complete, then tear down system
+    @test_containers.each { |thr| thr.wait }
+    @system_containers.each { |thr| thr.kill_that_thing! }
+  end
+
+  private
+  def build_image(build_path)
+    puts "Building #{build_path}"
+    image = Docker::Image.build_from_dir(build_path) do |line|
+      puts "#{build_path}: #{JSON.parse(line)['stream']}"
     end
-    runner_conf.fetch('system', []).each do |container_conf| 
-      threads << Thread.new do
-        build_path = container_conf['build']
-        puts "Building #{build_path}"
-        image = Docker::Image.build_from_dir(build_path)
-        puts "Running #{image}"
-        container = Docker::Container.create('Image' => image.id,
-          'Cmd' => [container_conf['cmd']],
-          'Volumes' => container_conf['volumes'],
-          'VolumesFrom' => container_conf['volumes-from']) 
-        container.tap(&:start).attach { |stream, chunk| puts "#{stream}: #{chunk}" }
-        container.wait
-        Thread.exit
+    return image
+  end
+
+  def create_container(conf, image)
+    createArgs = {'Image' => image.id}
+    createArgs['Cmd'] = conf['cmd'] if !conf['cmd'].nil?
+    createArgs['Volumes'] = conf['volumes'] if !conf['volumes'].nil?
+    createArgs['VolumesFrom'] = conf['volumes-from'] if !conf['volumes-from'].nil?
+    return Docker::Container.create(createArgs)
+  end
+
+  def start_container(conf, container)
+    puts "starting #{container}"
+    container.start('Privileged' => conf.fetch('privileged', false))
+    return container
+  end
+
+  def attach_container(container)
+    container.attach { |stream, chunk| puts "#{build_path}: #{chunk}" }
+  end
+
+  def start_and_attach(conf, container)
+    start_container(conf, container)
+    attach_container(container)
+  end
+
+  def thread_containers(confs, containers)
+    confs.each do |conf|
+      Thread.new do
+        container = create_container(conf, @images[conf['build']])
+        containers << container
+        start_and_wait(conf, container)
       end
     end
-    threads.each { |thr| thr.join }
   end
 end
-
