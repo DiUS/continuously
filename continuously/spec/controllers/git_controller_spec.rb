7f0c15c60090220b34a66aac14c96e89d654b26c
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-05-04 22:16:20 +1000

    first spec

--- continuously/spec/controllers/git_controller_spec.rb
+++ continuously/spec/controllers/git_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe GitController do
+  context 'called by github' do
+    before do
+      allow(File).to receive('directory?')
+      @mock_git = double(Git)
+      allow(Git).to receive('open').and_return @mock_git
+      allow(@mock_git).to receive('pull')
+      @request = {
+        repository: {
+          name: "test",
+          url: "http://whatever.com",
+          id: "239"
+        }
+      }
+    end
 
+    it 'should checkout cleanly for a new repo' do
+      expect(File).to receive('directory?').and_return false
+      expect(Git).to receive('clone')
+      post :create, payload: @request
+    end
+
+    it 'should update a checkout for a previously seen repo' do
+      expect(File).to receive('directory?').and_return true
+      expect(Git).to receive('open')
+      expect(@mock_git).to receive('pull')
+      post :create, payload: @request
+    end
+  end
 end
